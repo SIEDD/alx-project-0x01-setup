c6253e76a74ca461f2c879617da5b8a58f7df16a
siedd <saidmwacharo8@gmail.com>
2025-01-16 16:08:12 +0300

    post

--- alx-project-0x01/components/layout/Footer.tsx
+++ alx-project-0x01/components/layout/Footer.tsx
@@ -1,1 +1,10 @@
-
+const Footer: React.FC = () => {
+    return (
+      <footer className="bg-gray-800 text-white p-4">
+        <p>Footer Content</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,1 +1,11 @@
-
+const Users: React.FC = () => {
+    return (
+      <div className="p-8">
+        <h1 className="text-3xl font-semibold">Users</h1>
+        {/* Render User components here */}
+      </div>
+    );
+  }
+  
+  export default Users;
+  
