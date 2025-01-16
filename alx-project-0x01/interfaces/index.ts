693c699a1953197a4e4399f9e6f182450306221c
siedd <saidmwacharo8@gmail.com>
2025-01-17 00:03:09 +0300

    checker

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -1,4 +1,3 @@
-// PostProps interface for handling posts
 export interface PostProps {
   userId: number;
   id: number;
@@ -6,13 +5,11 @@ export interface PostProps {
   body: string;
 }
 
-// GeoProps interface for geo-location details
 export interface GeoProps {
   lat: string;
   lng: string;
 }
 
-// AddressProps interface for user address
 export interface AddressProps {
   street: string;
   suite: string;
@@ -21,14 +18,12 @@ export interface AddressProps {
   geo: GeoProps;
 }
 
-// CompanyProps interface for user company details
 export interface CompanyProps {
   name: string;
   catchPhrase: string;
   bs: string;
 }
 
-// UserProps interface for user details
 export interface UserProps {
   id: number;
   name: string;
@@ -40,7 +35,6 @@ export interface UserProps {
   company: CompanyProps;
 }
 
-// PostData interface for creating a new post
 export interface PostData {
   userId: number;
   id?: number;
@@ -48,13 +42,11 @@ export interface PostData {
   body: string;
 }
 
-// PostModalProps interface for the PostModal component
 export interface PostModalProps {
   onClose: () => void;
   onSubmit: (post: PostData) => void;
 }
 
-// UserData interface for user details (similar to UserProps)
 export interface UserData {
   id: number;
   name: string;
@@ -66,8 +58,7 @@ export interface UserData {
   company: CompanyProps;
 }
 
-// UserModalProps interface for the UserModal component
 export interface UserModalProps {
   onClose: () => void;
-  onSubmit: (user: UserProps) => void; // Ensure UserProps is used consistently
+  onSubmit: (user: UserProps) => void;
 }
